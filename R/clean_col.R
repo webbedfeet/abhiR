6a16ae35655ded8d29e4ecc33b7ea85b7121b5bf
Abhijit Dasgupta <aikidasgupta@gmail.com>
2018-05-16 08:49:52 -0400

    Initial commit

---
+++ R/clean_col.R
@@ -0,0 +1,22 @@
+#' Cleaning up a column for presentation output
+#'
+#' This function cleans up a column of text data, typically the first column, in a data.frame
+#' so that the same entry isn't repeated in the column as values in a second column, also a
+#' character or factor, changes.
+#'
+#' This function is meant to be useful in magrittr pipelines that use non-standard evaluation.
+#'
+#' @param x A data.frame or tibble object
+#' @param colvar The unquoted name of the column to transform
+#'
+#' @return
+#' @export
+#'
+#' @examples
+clean_col <- function(x, colvar){
+  require(dplyr)
+  colv <- enquo(colvar)
+  x %>% group_by(!!colv) %>% mutate(rown = row_number()) %>% ungroup() %>%
+    mutate_at(vars(!!colv), funs(ifelse(rown > 1, NA, .))) %>%
+    select (-rown)
+}
