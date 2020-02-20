6d8d656bf2653bca9b1acaaf91e938a781d3aea5
Abhijit Dasgupta <aikidasgupta@gmail.com>
2020-02-20 12:19:36 -0500

    Updated load_packages using pacman

--- R/load_packages.R
+++ R/load_packages.R
@@ -1,3 +1,23 @@
+#' Extracting package names from strings
+#'
+#' Options exist for Bioconductor and GitHub
+#'
+#' @param pkg Input string containing package name
+#'
+#' @return A string representing the package name
+extract_names <- function(pkg){
+  if(stringr::str_detect(pkg, 'BioC')){
+    pkg <- stringr::str_extract(pkg, '^\\w+')
+  }
+  if(stringr::str_detect(pkg, '/')){ # Github
+    pkg <- stringr::str_extract(pkg, '\\w+$')
+  }
+  return(pkg)
+}
+
+
+
+
 #' Loading project-required packages in R
 #'
 #' This function reads a file "pkgs.yml" in the `lib` directory of the project
@@ -10,45 +30,45 @@
 #' @export
 #'
 #' @examples
-load_packages <- function(homedir = '.') {
+load_packages <- function(homedir = here::here()) {
+  require(pacman)
   `%notin%` <- Negate('%in%')
   rversion <- paste(version$major, version$minor, sep='.')
-  if(!file.exists(file.path('lib','pkgs.yml'))){
+  if(!file.exists(here::here('lib','pkgs.yml'))){
     add_package('abhiR')
   }
-  pkgs <- yaml::yaml.load_file(file.path('lib','pkgs.yml'))
+  pkgs <- yaml::yaml.load_file(here::here('lib','pkgs.yml'))
+
+  pkg_names <- purrr::map_chr(pkgs, extract_names)
+  pkgs1 <- pkg_names[!purrr::map_lgl(pkg_names, pacman::p_isinstalled)] # What's not installed
   if(all(stringr::str_detect(pkgs, '=>'))){# Version information included
     out = do.call(rbind,stringr::str_split(pkgs,'=>'))
     pkgs <- out[,1]
     versions <- out[,2]
   } else {versions <- NULL}
-  for(p in pkgs){
+  # Installing missing packages
+  for(p in pkgs1){
     if (stringr::str_detect(p, 'bioC')){ # Bioconductor packages
-      pkgname <- stringr::str_extract(p, '^[a-zA-Z0-9]+')
-      if (pkgname %notin% installed.packages()[,1]){
-        if(rversion < '3.5'){
-          source('http://bioconductor.org/biocLite.R')
-          biocLite(pkgname)
-        } else {
-          BiocManager::install(pkgname)
-        }
-        suppressPackageStartupMessages(library(pkgname, character.only = TRUE))
-      }
+      pkgname <- extract_name(p)
+      p_install(pkgname, try.bioconductor = TRUE)
+      # if(rversion < '3.5'){
+      #   source('http://bioconductor.org/biocLite.R')
+      #   biocLite(pkgname)
+      # } else {
+      #   BiocManager::install(pkgname)
+      # }
     } else if (stringr::str_detect(p, '/')){ # GitHub packages
-      pkgname = stringr::str_split(p, '/')[[1]][[2]]
-      if (pkgname %notin% installed.packages()[,1]){
-        devtools::install_github(p)
-      }
-      suppressPackageStartupMessages(library(pkgname, character.only = TRUE))
+      pkgname <- extract_names(p)
+      p_install_gh(p)
+      # devtools::install_github(p)
     } else { # CRAN packages
-      if(!(p %in% installed.packages()[,1])){
-        if(!is.null(versions)){
-          devtools::install_version(p, version = out[out[,1]==p,2], repos = 'http://cran.rstudio.com')
-        } else{
-          utils::install.packages(p, repos='http://cran.rstudio.com')
-        }
+      if(!is.null(versions)){
+        devtools::install_version(p, version = out[out[,1]==p,2], repos = 'http://cran.rstudio.com')
+      } else{
+        p_install(p)
+        # utils::install.packages(p, repos='http://cran.rstudio.com')
       }
-      suppressPackageStartupMessages(library(p, character.only = TRUE))
     }
   }
+  p_load(char = pkgs)
 }
