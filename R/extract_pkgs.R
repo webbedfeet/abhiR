4b060b636e53224508c89a23bd8bcedfac34b1f6
Abhijit Dasgupta <aikidasgupta@gmail.com>
2020-07-10 01:42:27 -0400

    added package::name

--- R/extract_pkgs.R
+++ R/extract_pkgs.R
@@ -9,19 +9,19 @@ extract_pkgs <- function(){
   require(here)
   require(tidyverse)
 
-  codefiles <- dir_ls(here(), regex = 'R[md]*$', recurse=TRUE, type='file')
-  a <- map(codefiles, ~grep('library\\(.*\\)', readLines(.), value=TRUE))
-  b <- map(codefiles, ~grep('p_load', readLines(.), value=TRUE)) %>%
+  codefiles <- fs::dir_ls(here::here(), regex = 'R[md]*$', recurse=TRUE, type='file')
+  a <- purrr::map(codefiles, ~grep('library\\(.*\\)', readLines(.), value=TRUE))
+  b <- purrr::map(codefiles, ~grep('p_load', readLines(.), value=TRUE)) %>%
     unlist() %>% unname()
-  d <- map(codefiles, ~grep('require\\(', readLines(.), value=TRUE))
+  d <- purrr::map(codefiles, ~grep('require\\(', readLines(.), value=TRUE))
 
-  pkg_a <- map(a, ~str_remove(.x, '#.*')) %>%
-    map(~str_extract_all(.x, '[\\w\\.]+')) %>%
-    modify_depth(2, ~.x[length(.x)]) %>%
+  pkg_a <- purrr::map(a, ~str_remove(.x, '#.*')) %>%
+    purrr::map(~stringr::str_extract_all(.x, '[\\w\\.]+')) %>%
+    purrr::modify_depth(2, ~.x[length(.x)]) %>%
     unlist() %>% unname() %>%
-    tibble('x' = .) %>%
-    filter(str_detect(x, '_+', negate=TRUE)) %>%
-    pull(x)
+    tibble::tibble('x' = .) %>%
+    dplyr::filter(stringr::str_detect(x, '_+', negate=TRUE)) %>%
+    dplyr::pull(x)
 
   b1 <- b[str_detect(b,'char')]
   b2 <- b[str_detect(b, 'char', negate=TRUE)]
